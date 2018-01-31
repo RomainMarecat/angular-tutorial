ca71130f8e8b02fbe310a50f06d009890e26a802
Romain Marecat <romain.marecat@gmail.com>
2018-01-31 18:09:45 +0100

    Improved service tuto

--- src/app/shared/recipe/recipe.service.ts
+++ src/app/shared/recipe/recipe.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Recipe } from './recipe';
 import { SimpleRecipe } from './simple-recipe';
+import 'rxjs/add/observable/of';
+import { map, filter } from 'rxjs/operators';
 
 @Injectable()
 export class RecipeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getRecipes(): Observable<Array<Recipe>> {
-    return;
+  getRecipes() {
+    this.http.get<Array<SimpleRecipe>>(`assets/data/recipe.json`)
+      .pipe(
+      map((recipes: Array<Recipe>) => recipes),
+      filter((recipes: Array<SimpleRecipe>) => recipes
+
+          /*recipes.map((recipe: SimpleRecipe) => {
+            return this.recipe.name.includes('Pizza');
+          })*/
+          )
+      );
   }
 }
